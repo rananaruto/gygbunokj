6e287446b983a208a31f620cc89ff55ad2b9c1d9
deevsaini <deevsaini@github.com>
2026-05-06 18:32:38 +0500

    fix: switch to stable ffmpeg_kit_flutter and fix missing imports

--- lib/controllers/download_controller.dart
+++ lib/controllers/download_controller.dart
@@ -4,6 +4,8 @@ import 'package:get/get.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:youtube_explode_dart/youtube_explode_dart.dart';
+import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
+import 'package:ffmpeg_kit_flutter/return_code.dart';
 import 'package:snaptube/controllers/history_controller.dart';
 import 'package:snaptube/models/download_task_model.dart';
 import 'package:snaptube/models/history_item_model.dart';
